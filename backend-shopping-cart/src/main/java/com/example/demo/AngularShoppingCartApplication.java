dcb2affe319c274f09d66b8dfaf744f99b734eb9
ngo tan <tanhaingan@gmail.com>
2022-11-27 16:44:17 +0700

    update lại main

--- backend-shopping-cart/src/main/java/com/example/demo/AngularShoppingCartApplication.java
+++ backend-shopping-cart/src/main/java/com/example/demo/AngularShoppingCartApplication.java
@@ -10,7 +10,9 @@ public class AngularShoppingCartApplication {
 		SpringApplication.run(AngularShoppingCartApplication.class, args);
 		System.out.println("Haha");
 		System.out.println("Ho ho");
-
+		
+		if(true){
 		System.out.println("a+b=c");
+		}
 	}
 }
